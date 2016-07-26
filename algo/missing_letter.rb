6006728674fdddb6985c0848b699ae36d7183842
Ramiro Trejo <trejoramiro1@gmail.com>
2016-07-26 17:46:45 -0500

    Solutions to haystack.rb and missing_letter.rb

--- algo/missing_letter.rb
+++ algo/missing_letter.rb
@@ -3,7 +3,26 @@
 # bonus: returns a string of all missing letters as a string. ex: find_missing_letter("ace") would return "bd", write your own test.
 
 def find_missing_letter(range)
+  missing_letters = ""
 
+  n = range.length - 1
+  prev_ascii = range[0].ord
+
+  range.split("")[1..n].each do |char|
+    if char.ord == (prev_ascii + 1 )
+      prev_ascii = char.ord
+    else
+      missing_char = (prev_ascii + 1).chr
+      missing_letters = missing_letters + missing_char
+      prev_ascii = char.ord
+    end
+  end
+
+  if missing_letters.length == 0
+    missing_letters = nil
+  end
+
+  return missing_letters
 end
 
 # Driver code - don't touch anything below this line.
@@ -29,6 +48,18 @@ puts result
 puts
 
 if result == nil
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = find_missing_letter("ace")
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == "bd"
   puts "PASS!"
 else
   puts "F"
