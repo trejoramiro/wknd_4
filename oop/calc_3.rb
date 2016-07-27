574f4fd029740d3b5d8a10e6a1cd55623e0b9586
Ramiro Trejo <trejoramiro1@gmail.com>
2016-07-27 15:42:32 -0500

    Solution to calc_3.rb

--- oop/calc_3.rb
+++ oop/calc_3.rb
@@ -1,6 +1,6 @@
 # In this example, we have THREE calculators, the two included in the previous
 # exercises as well as Whiz Bang Calculator that can even calculate the hypotenuse
-# of a triangle if given the length of the other two sides. Figure out how to 
+# of a triangle if given the length of the other two sides. Figure out how to
 # DRY up all the code below - there shouldn't be a single method duplicated between
 # any two classes.
 
@@ -24,23 +24,23 @@ class SimpleCalculator
 
 end
 
-class FancyCalculator
+class FancyCalculator < SimpleCalculator
 
-  def add(first_number, second_number)
-    first_number + second_number
-  end
+  # def add(first_number, second_number)
+  #   first_number + second_number
+  # end
 
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
+  # def subtract(first_number, second_number)
+  #   first_number - second_number
+  # end
 
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
+  # def multiply(first_number, second_number)
+  #   first_number * second_number
+  # end
 
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
+  # def divide(first_number, second_number)
+  #   first_number / second_number
+  # end
 
   def square_root(number)
     Math.sqrt(number)
@@ -48,32 +48,32 @@ class FancyCalculator
 
 end
 
-class WhizBangCalculator
+class WhizBangCalculator < FancyCalculator
 
-  def add(first_number, second_number)
-    first_number + second_number
-  end
+  # def add(first_number, second_number)
+  #   first_number + second_number
+  # end
 
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
+  # def subtract(first_number, second_number)
+  #   first_number - second_number
+  # end
 
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
+  # def multiply(first_number, second_number)
+  #   first_number * second_number
+  # end
 
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
+  # def divide(first_number, second_number)
+  #   first_number / second_number
+  # end
 
-  def square_root(number)
-    Math.sqrt(number)
-  end
+  # def square_root(number)
+  #   Math.sqrt(number)
+  # end
 
   def hypotenuse(first_number, second_number)
     Math.hypot(first_number, second_number)
   end
-  
+
   def exponent(first_number, exponent_number)
     total = 1
     exponent_number.times { total = multiply(total,first_number) }
@@ -84,3 +84,73 @@ end
 
 # Copy your driver code from the previous exercise and more below:
 
+puts "Testing Stats..."
+puts
+
+# Write your own driver code below:
+calc = FancyCalculator.new()
+calc2 = WhizBangCalculator.new()
+
+puts "Testing addition..."
+puts
+
+if calc.add(2,2) == 4
+  puts "PASS"
+else
+  puts "FAIL"
+end
+
+puts "Testing subtraction..."
+puts
+
+if calc.subtract(5,2) == 3
+  puts "PASS"
+else
+  puts "FAIL"
+end
+
+puts "Testing multiply..."
+puts
+
+if calc.multiply(5,2) == 10
+  puts "PASS"
+else
+  puts "FAIL"
+end
+
+puts "Testing division..."
+puts
+
+if calc.divide(6,2) == 3
+  puts "PASS"
+else
+  puts "FAIL"
+end
+
+puts "Testing square root..."
+puts
+
+if calc.square_root(4) == 2
+  puts "PASS"
+else
+  puts "FAIL"
+end
+
+puts "Testing hypotenuse..."
+puts
+
+if calc2.hypotenuse(3,4) == 5
+  puts "PASS"
+else
+  puts "FAIL"
+end
+
+
+puts "Testing exponent..."
+puts
+
+if calc2.exponent(5,3) == 125
+  puts "PASS"
+else
+  puts "FAIL"
+end
